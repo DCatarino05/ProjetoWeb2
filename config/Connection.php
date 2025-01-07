a1aacc3f6bfb463a83f22b9dc513b47fc40c1ea0
David Catarino <93658234+DCatarino05@users.noreply.github.com>
2025-01-07 13:30:06 +0000

    teste

--- config/Connection.php
+++ config/Connection.php
@@ -7,17 +7,23 @@ class Connection
     {
         if (self::$instance === null) {
             try {
+                // Substitua os parâmetros abaixo pelos dados fornecidos pelo servidor remoto
+                $host = 'mysql.php-myadmin.net'; // Verifique o hostname correto
+                $dbname = 'if0_37857865_cinemasnosso';
+                $user = 'if0_37857865'; // Substitua pelo seu nome de usuário
+                $password = '57e1HKRVlNP8l '; // Substitua pela sua senha
+
                 self::$instance = new PDO(
-                    'mysql:host=localhost;dbname=cinemasnosso', 
-                    'root', 
-                    '', 
+                    "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+                    $user,
+                    $password,
                     [
                         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
                         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC 
                     ]
                 );
             } catch (PDOException $e) {
-                die("ERRO DE CONEXÃO : " . $e->getMessage());
+                die("ERRO DE CONEXÃO: " . $e->getMessage());
             }
         }
 
